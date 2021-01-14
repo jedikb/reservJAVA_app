cda9bdb9404f6838871bb908893a798d8ae06734
최민혁 <choimin1136@gmail.com>
2021-01-14 14:06:59 +0900

    매장화면 상품목록 리스트 구현  - StoreActivity RecyclerView추가  - Adapter 추가  - item_product.xml추가

---
+++ ReservJAVA_app/app/src/main/java/com/example/reservjava_app/adapter/StoreMenuAdapter.java
@@ -0,0 +1,69 @@
+package com.example.reservjava_app.adapter;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.reservjava_app.DTO.ProductDTO;
+import com.example.reservjava_app.R;
+
+import java.util.ArrayList;
+
+public class StoreMenuAdapter extends RecyclerView.Adapter<StoreMenuAdapter.ViewHolder> {
+
+    //adapter에 들어갈 list
+    private ArrayList<ProductDTO> pdList = new ArrayList<>();
+
+    //아이템 뷰를 정장하는 ViewHolder.Class
+    public class ViewHolder extends RecyclerView.ViewHolder {
+
+            private TextView product_name;
+            private TextView product_info;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            product_name = itemView.findViewById(R.id.product_name);
+            product_info = itemView.findViewById(R.id.product_info);
+
+        }
+
+        public void onBind(ProductDTO pddto){
+            product_name.setText(pddto.getProduct_name());
+            product_info.setText(pddto.getProduct_info());
+        }
+
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        //item_product.xml 을 inflate
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.item_product, parent, false);
+
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        //item  bind 함수
+        holder.onBind(pdList.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        // RecyclerView 개수
+        return pdList.size();
+    }
+
+    //아이템 추가 메소드
+    public void addItem(ProductDTO productDTO){
+        pdList.add(productDTO);
+    }
+
+}
--- ReservJAVA_app/app/src/main/java/com/example/reservjava_app/reservation/Store.java
+++ ReservJAVA_app/app/src/main/java/com/example/reservjava_app/reservation/Store.java
@@ -1,16 +1,33 @@
 package com.example.reservjava_app.reservation;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.Adapter;
+import android.widget.LinearLayout;
 
 import com.example.reservjava_app.R;
+import com.example.reservjava_app.adapter.StoreMenuAdapter;
 
 public class Store extends AppCompatActivity {
+
+    StoreMenuAdapter adapter;
+    RecyclerView recyclerView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_store);
+
+        recyclerView = findViewById(R.id.store_recycler);
+
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(linearLayoutManager);
+
+        adapter = new StoreMenuAdapter();
+        recyclerView.setAdapter(adapter);
     }
 }
