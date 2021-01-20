4c1c89aaae4028fd17989f6fa22e85af51064a4f
302-04\hanul <vjr141515@gmail.com>
2021-01-20 20:11:31 +0900

    아이디와 비밀번호 찾는 화면 만들었습니다^^ 아직 기능 구현은 안했습니다!

---
+++ ReservJAVA_app/app/src/main/java/com/example/reservjava_app/ui/a_login_signup/PwSearch.java
@@ -0,0 +1,17 @@
+package com.example.reservjava_app.ui.a_login_signup;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.reservjava_app.R;
+
+public class PwSearch extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pwsearch);
+
+    }
+
+}
