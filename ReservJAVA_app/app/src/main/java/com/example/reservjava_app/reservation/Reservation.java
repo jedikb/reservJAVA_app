3b6f49132b16e6801907fc50ae9922fe8347c7f1
302-03\hanul <choimin1136@gmail.com>
2021-01-22 16:46:12 +0900

    (임시) Store 화면에서 Reservation 화면으로 intent

--- ReservJAVA_app/app/src/main/java/com/example/reservjava_app/reservation/Reservation.java
+++ ReservJAVA_app/app/src/main/java/com/example/reservjava_app/reservation/Reservation.java
@@ -1,16 +1,48 @@
 package com.example.reservjava_app.reservation;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
 import com.example.reservjava_app.R;
+import com.example.reservjava_app.adapter.ExpandableListAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Reservation extends AppCompatActivity {
+    private RecyclerView recyclerview;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reservation);
+
+        recyclerview = findViewById(R.id.recyclerview);
+        recyclerview.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false));
+        List<ExpandableListAdapter.Item> data = new ArrayList<>();
+
+        data.add(new ExpandableListAdapter.Item(ExpandableListAdapter.HEADER, "날짜"));
+        data.add(new ExpandableListAdapter.Item(ExpandableListAdapter.CHILD, "Apple"));
+        data.add(new ExpandableListAdapter.Item(ExpandableListAdapter.CHILD, "Orange"));
+        data.add(new ExpandableListAdapter.Item(ExpandableListAdapter.CHILD, "Banana"));
+        data.add(new ExpandableListAdapter.Item(ExpandableListAdapter.HEADER, "시간"));
+        data.add(new ExpandableListAdapter.Item(ExpandableListAdapter.CHILD, "Audi"));
+        data.add(new ExpandableListAdapter.Item(ExpandableListAdapter.CHILD, "Aston Martin"));
+        data.add(new ExpandableListAdapter.Item(ExpandableListAdapter.CHILD, "BMW"));
+        data.add(new ExpandableListAdapter.Item(ExpandableListAdapter.CHILD, "Cadillac"));
+
+        ExpandableListAdapter.Item places = new ExpandableListAdapter.Item(ExpandableListAdapter.HEADER, "인원");
+        places.invisibleChildren = new ArrayList<>();
+        places.invisibleChildren.add(new ExpandableListAdapter.Item(ExpandableListAdapter.CHILD, "Kerala"));
+        places.invisibleChildren.add(new ExpandableListAdapter.Item(ExpandableListAdapter.CHILD, "Tamil Nadu"));
+        places.invisibleChildren.add(new ExpandableListAdapter.Item(ExpandableListAdapter.CHILD, "Karnataka"));
+        places.invisibleChildren.add(new ExpandableListAdapter.Item(ExpandableListAdapter.CHILD, "Maharashtra"));
+
+        data.add(places);
+
+        recyclerview.setAdapter(new ExpandableListAdapter(data));
     }
 }
