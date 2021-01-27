835f0ed5b8b8b239798e86b66107376a2422cc1c
LeeBongSun <lbs0511@gmail.com>
2021-01-27 20:31:34 +0900

    Manifest 수정

--- ReservJAVA_app/app/src/main/java/com/example/reservjava_app/ui/d_bongsun/BookingViewActivity.java
+++ ReservJAVA_app/app/src/main/java/com/example/reservjava_app/ui/d_bongsun/BookingViewActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.example.reservjava_app.R;
+
 public class BookingViewActivity extends AppCompatActivity {
 
     @Override
