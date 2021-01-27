e9c4218d76d20ccacaf7125a01a7c6e7dd6ea77b
LeeBongSun <lbs0511@gmail.com>
2021-01-27 20:30:41 +0900

    BookingViewActivity, MemberCancelActivity, PaymentActivity 추가

---
+++ ReservJAVA_app/app/src/main/java/com/example/reservjava_app/ui/d_bongsun/MemberCancelActivity.java
@@ -0,0 +1,14 @@
+package com.example.reservjava_app.ui.d_bongsun;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MemberCancelActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_member_cancel);
+    }
+}
