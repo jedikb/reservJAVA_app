195a14eca087016d074f14ccb2b521f20c671dd1
302-04\hanul <vjr141515@gmail.com>
2021-01-29 17:58:13 +0900

    또 쓸데없는 창들 다 지우고 아이디 비번 찾기 페이지 새로 만들어서 인텐트 해놨어용! 일단 페이지 연동들만 시켜놨습니당 ^ㅇ^ 나머진 담주에 ing

--- ReservJAVA_app/app/src/main/java/com/example/reservjava_app/ui/a_login_signup/PwSearchActivity.java
+++ ReservJAVA_app/app/src/main/java/com/example/reservjava_app/ui/a_login_signup/PwSearchActivity.java
@@ -1,17 +1,16 @@
 package com.example.reservjava_app.ui.a_login_signup;
 
-import android.os.Bundle;
-
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
 
 import com.example.reservjava_app.R;
 
-public class PwSearch extends AppCompatActivity {
+public class PwSearchActivity extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_pwsearch);
-
+        setContentView(R.layout.activity_pw_search);
     }
-
 }
