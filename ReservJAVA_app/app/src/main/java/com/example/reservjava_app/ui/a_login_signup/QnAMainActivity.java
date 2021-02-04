177762cd0ffa3ddb50fbd34dc0f7b127ce1b7d46
302-04\hanul <vjr141515@gmail.com>
2021-02-04 19:45:26 +0900

    문의하기 메임화면에도 햄버거바 띄워놨습니당~^^

--- ReservJAVA_app/app/src/main/java/com/example/reservjava_app/ui/a_login_signup/QnAMainActivity.java
+++ ReservJAVA_app/app/src/main/java/com/example/reservjava_app/ui/a_login_signup/QnAMainActivity.java
@@ -1,6 +1,9 @@
 package com.example.reservjava_app.ui.a_login_signup;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -15,11 +18,22 @@ import com.example.reservjava_app.ui.d_bongsun.MemberCancelActivity;
 public class QnAMainActivity extends AppCompatActivity {
 
     Button qnabtn, drawalbtn;
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_qn_a_main);
+
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        //위에 툴바 생기게 해서 네비게이션드로어 띄우고 닫을수 있게 하는 기능!
+        toolbar = findViewById(R.id.toolbar);
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
 
         qnabtn = findViewById(R.id.qnabtn);
         //문의하기 화면띄우기
@@ -51,4 +65,5 @@ public class QnAMainActivity extends AppCompatActivity {
         });
 
     }
+
 }
